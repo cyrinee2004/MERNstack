3455cbe507f5fdd256817ce0be1f05d32388d66b
Cyrine04 <midassicyrine@gmail.com>
2025-07-08 21:58:30 +0100

    scnd vd pt1

---
+++ global.js
@@ -0,0 +1,14 @@
+//console.log(global);
+// setTimeout(() => {
+//     console.log('In the timeout');
+//     clearInterval(int); // Clear the interval after 3 seconds
+//     }, 3000);
+
+// const int= setInterval(() => {
+//     console.log('In the interval');
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+console.log(document.querySelector);
