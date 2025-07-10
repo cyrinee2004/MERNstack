4230338a712d0239771e201ad82d17601a4546e2
Cyrine04 <midassicyrine@gmail.com>
2025-07-10 01:04:57 +0100

    tut4

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log("request made ");
+    console.log(req.url, req.method);
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
 
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200; // OK
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200; // OK
+            break;
+        case '/about-me':
+            res.statusCode = 301; // Moved Permanently
+            res.setHeader('Location', '/about');
+            res.end();
+        default:
+            path += '404.html';
+            res.statusCode = 404; // Not Found
+            break;
+    }
+
+    // send response
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            //res.write( data );
+            res.end(data);
+        }
+    })
 
 });
 server.listen(3000, 'localhost', () => {
