4eebf1626bb51ac70bd19936eff647746272d56a
Cyrine04 <midassicyrine@gmail.com>
2025-07-17 17:51:49 +0100

    tut5

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    // lodash example
+    const num = _.random(0, 20);
+    console.log(num);   
+    const greet = _.once(() => {
+        console.log('Hello, World!');
+    });
+    greet();
+
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
@@ -16,7 +24,7 @@ const server = http.createServer((req, res) => {
             path += 'about.html';
             res.statusCode = 200; // OK
             break;
-        case '/about-me':
+        case '/about-us':
             res.statusCode = 301; // Moved Permanently
             res.setHeader('Location', '/about');
             res.end();
