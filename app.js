aa659deaab3295d34e95d49419f50290a5579060
Cyrine04 <midassicyrine@gmail.com>
2025-07-24 09:13:55 +0100

    tut7

--- app.js
+++ app.js
@@ -3,24 +3,33 @@ const express = require('express');
 // Create an Express application
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
+
 //listen for requests on port 3000
 app.listen(3000);
+
+
 app.get('/', (req, res) => {
-    //res.send('<h1>Home Page</h1>');
-    res.sendFile('./views/index.html', { root: __dirname });
-});
-app.get('/about', (req, res) => {
-    //res.send('<h1>about Page</h1>');
-    res.sendFile('./views/about.html', { root: __dirname });
-});
-// Redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+     const blogs = [
+        { title: 'Blog 1', snippet: 'This is blog 1' },
+        { title: 'Blog 2', snippet: 'This is blog 2' },
+        { title: 'Blog 3', snippet: 'This is blog 3' }
+    ];
+    res.render('index', { title: 'Home', blogs, name: 'Blog Ninja' });
 });
 
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });   
+});
 //error handling
 app.use((req, res) => {
-    res.status(400).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404' });
 });
 
 
