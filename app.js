864fae0571d3db2b6c8d73fd5240eb80ee8127fc
Cyrine04 <midassicyrine@gmail.com>
2025-07-24 12:14:46 +0100

    tut9

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog'); // Import the Blog model
 
 // Create an Express application
 const app = express();
+
+// MongoDB connection string (replace <db_password> with your actual password)
+const dbuRI='mongodb+srv://netninja:cyrine.04@cluster0.9vhlkvc.mongodb.net/'
+mongoose.connect(dbuRI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch(err => console.error('Could not connect to MongoDB:', err));
+
 
 //register view engine
 app.set('view engine', 'ejs');
 
 
-//listen for requests on port 3000
-app.listen(3000);
-
 //middleware and static files
 app.use(express.static('public'));
-
 app.use(morgan('dev'));
 
 
+//routes
 app.get('/', (req, res) => {
-     const blogs = [
-        { title: 'Blog 1', snippet: 'This is blog 1' },
-        { title: 'Blog 2', snippet: 'This is blog 2' },
-        { title: 'Blog 3', snippet: 'This is blog 3' }
-    ];
-    res.render('index', { title: 'Home', blogs, name: 'Blog Ninja' });
+ res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
+
+// Blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All blogs', blogs: result });
+        })
+        .catch((err) =>{
+             console.error(err)
+        });
+}); 
+
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' });   
